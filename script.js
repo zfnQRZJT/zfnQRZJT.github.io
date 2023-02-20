9c7587498fbdb37053dd84c5d109e18445f8c35f
zfnQRZJT <102266031+zfnQRZJT@users.noreply.github.com>
2023-02-20 11:10:32 -0800

    Fix zfnmistake

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ const find = function(elem) {
   return(document.getElementById(elem));
 }
 const startBoard = function() {
-  find("pixelboard").innerHTML = ("<div class='pixelRow'>" + ("<div class='pixel'></div>).repeat(64) + "</div>").repeat(64);
+  find("pixelboard").innerHTML = ("<div class='pixelRow'>" + ("<div class='pixel'></div>").repeat(64) + "</div>").repeat(64);
 }
 window.onload = startBoard;
