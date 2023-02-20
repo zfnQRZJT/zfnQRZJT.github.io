aa60256ec9cd999211fd76aa94b76857ba16a316
zfnQRZJT <102266031+zfnQRZJT@users.noreply.github.com>
2023-02-20 11:02:39 -0800

    Preliminary js board maker

---
+++ script.js
@@ -0,0 +1,7 @@
+const find = function(elem) {
+  return(document.getElementById(elem));
+}
+const startBoard = function() {
+  find("pixelboard").innerHTML = ("<div class='pixelRow'>" + ("<div class='pixel'></div>).repeat(64) + "</div>").repeat(64);
+}
+window.onload = startBoard;
