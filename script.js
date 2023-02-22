b198b6cbee879950b2e8f29a02ce155f0f60d7ee
zfnQRZJT <102266031+zfnQRZJT@users.noreply.github.com>
2023-02-21 16:23:51 -0800

    Yet more logging

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ var intervals = [];
 var interval = 0;
 const setImage = function(imgdata) {
   interval = setInterval(function(intid) {
-    console.log(intid);
+    console.log(intid,intervals);
     if (intervals[intid] <= 63) {
       for (let i = 0; i <= intervals[intid]; i++) {
         find("pixelboard").children[i].children[intervals[intid] - i].style.background = imgdata[i][intervals[intid] - i];
