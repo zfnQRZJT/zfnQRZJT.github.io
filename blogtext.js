483c63e9172d16402b13454f7557a9a542af8705
zfnQRZJT <102266031+zfnQRZJT@users.noreply.github.com>
2023-02-27 22:15:36 -0800

    Figured out f(2)

--- blogtext.js
+++ blogtext.js
@@ -1,6 +1,6 @@
 const blogs = {
   "6353227688162363":["Surely this simple recursive function won't return ridiculous results?",
-"A friend sent an image of this unassuming functional equation (calling it 'hell')<br>\
+"A friend sent an image of this unassuming functional equation (calling it \"hell\")<br>\
 \\(f(x) = -x\\) for x < 0,<br>\
 \\(\\frac{1}{2}f(x-f(x-1))\\) otherwise,<br>\
 find \\(f(3)\\).<br><br><br>\
@@ -37,14 +37,20 @@ Now let's find a closed form for \\(f(2 - \\frac{n}{2^{x}})\\).<br>\
 \\(f(2 - \\frac{n}{2^{x}}) = \\frac{1}{2}f(2 - \\frac{n}{2^{x}} - \\frac{n - 2^{y}}{2^{x}} = \\frac{1}{2}f(2 - \\frac{2n - 2^{y}}{2^x})\\).<br><br>\
 It may be difficult to understand what exactly the transformation inside the function is that gives a coefficient of 1/2 outside the function. In the range [0,1) it was easy to understand: 2x - 1 doubles the distance from 1. What is this one doing?<br>\
 Let's look at some examples.<br>\
-(I will ignore the 2 - for now)<br>\
+(I will ignore the \"2 -\" part for now)<br>\
 \\(\\frac{141}{256} \\Rightarrow \\frac{154}{256} \\Rightarrow \\frac{180}{256} \\Rightarrow \\frac{232}{256} \\Rightarrow \\frac{336}{256} \\).<br><br>\
-It's very unclear what this is doing. It finds the largest power of 2 less than n, then subtracts from 2n that power? It's easier to understand if I write it as n + n - 2^y. This is actually doubling n's distance from that power. (n starts odd by definition so it will never be a power of 2.)<br><br>\
+It's very unclear what this is doing. It finds the largest power of 2 less than n, then subtracts from 2n that power? It's easier to understand if I write it as n + n - 2^y. This is actually doubling n's distance from that power.<br><br>"/*
+If n is a power of 2 this is actually easy. It will double the distance from n/2, leaving 3n/2, then it will double the distance from n, leaving 2n, and in general it will double every 2 steps. <br>\
 Now here's where <b>writing n in binary</b> will be a lifesaver. Given some number, like 10100001, this transformation leaves the first digit untouched, and doubles all digits after it.<br><br>\
 If n > 2^x/2 (as it is in the case of 141/256) it simply surpasses 2^x after 1 more than the number of 0's iterations. (Multiplying by 2 in binary moves a section of digits left 1 and adds a 0 at the end, just like multiplying by 10 works in base 10.) Let's write that as \\(y - L2(n - 2^{y})\\)<br><br>\
 If n < 2^x/2 it is a bit more complicated. Let's write n in binary as 1 [a zeroes] 1...<br>\
-After a doublings this will come to 11...<br>\
-"]
+After a doublings this will come to 11...<br>\*/+
+"Understanding the truly amazing complexity of this transformation is critical for the solution of this problem. But we can actually simplify for now.<br>\
+n starts as a power of 2, namely 1, in 2 - 1/8. In iteration 1 it will double the distance from n/2, giving 3n/2. Then it will double the distance from n, giving 2n. In general, it will double its distance from 2 every 2 steps, giving an additional multiplication of 1/4.<br>\
+We know that \\(f(2) = \\frac{1}{2}f(15/8)\\). We can now see that this is \\(\\frac{1}{2}f(15/8) = \\frac{1}{8}f(14/8) = \\frac{1}{32}f(12/8) = \\frac{1}{128}f(8/8) = \\frac{1}{128}f(1) = \\frac{1}{128}\\frac{1}{8} = \\frac{1}{1024}\\).]<br>\
+In general, \\(f(2 - \\frac{1}{2^{x}}) = \\frac{1}{2^{2x + 3}}\\).<br>\
+<b>f(2) = 1/1024.</b><br><br><br>\
+Now it's time for our final stand. \\(f(2) = \\frac{1}{1024}\\). \\(f(3) = \\frac{1}{2}f(3 - \\frac{1}{1024}) = \\frac{1}{2}f(\\frac{3071}{1024})\\). This will relate to \\(f(2 - 1/1024)\\)
 }
 const urlPath = (new URL(window.location.href).search.substr(1));
 window.onload = function() {
