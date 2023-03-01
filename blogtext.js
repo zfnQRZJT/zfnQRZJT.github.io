7a33255ea2c9097b6737ee2014ce8131badc7a64
zfnQRZJT <102266031+zfnQRZJT@users.noreply.github.com>
2023-02-28 17:00:03 -0800

    Convert a lot of math to zfnmath

--- blogtext.js
+++ blogtext.js
@@ -1,37 +1,41 @@
+const half = "<frac><n>1</n><d>2</d></frac>";
+const frac = function(n,d) {
+  return("<frac><n>" + n + "</n><d>" + d + "</d></frac>");
+}
 const blogs = {
   "6353227688162363":["Surely this simple recursive function won't return ridiculous results?",
-"A friend sent an image of this unassuming functional equation (calling it \"hell\")<br>\
+`A friend sent an image of this unassuming functional equation (calling it \"hell\")<br>\
 <m>f(x) = -x</m> for <m>x < 0</m>,<br>\
-<m><frac><n>1</n><d>2</d></frac>f(x-f(x-1))</m> otherwise,<br>\
+<m>${half}f(x-f(x-1))</m> otherwise,<br>\
 find <m>f(3)</m>.<br><br><br>\
-My first thought was, since it is defined for negative numbers, let's find <m>f(0))</m>, then <m>f(1)</m>, then <m>f(2)</m>, then <m>f(3)</m>.<br>\
+My first thought was, since it is defined for negative numbers, let's find <m>f(0)</m>, then <m>f(1)</m>, then <m>f(2)</m>, then <m>f(3)</m>.<br>\
 In essence this is what we will do. But let's just try doing it.<br>\
-<m>f(0)=\\frac{1}{2}f(0-f(-1)) = \\frac{1}{2}f(-1)=\\frac{1}{2}</m><br><br>\
+<m>f(0)=${half}f(0-f(-1)) = ${half}f(-1)=${half}</m><br><br>\
 <b>Then f(1)...</b><br>\
-<m>f(1)=\\frac{1}{2}f(1-f(0))=\\frac{1}{2}f(\\frac{1}{2})</m><br>\
+<m>f(1)=${half}f(1-f(0))=${half}f(${half})</m><br>\
 Oh, it looks like we need f(1/2) first.<br>\
-<m>f(\\frac{1}{2})=\\frac{1}{2}f(\\frac{1}{2}-f(-\\frac{1}{2}))=\\frac{1}{2}f(\\frac{1}{2}-\\frac{1}{2})=\\frac{1}{2}f(0)=\\frac{1}{4}</m><br>\
-So <m>f(1)=\\frac{1}{2}\\frac{1}{4} = \\frac{1}{8}</m>.<br><br>\
+<m>f(${half})=${half}f(${half}-f(-${half}))=${half}f(${half}-${half})=${half}f(0)=${frac(1,4)}</m><br>\
+So <m>f(1)=${half}${frac(1,4)} = ${frac(1,8)}</m>.<br><br>\
 <b>Now f(2)...</b><br>\
-<m>f(2) = \\frac{1}{2}f(2 - f(1)) = \\frac{1}{2}f(2 - \\frac{1}{8}) = \\frac{1}{2}f(\\frac{15}{8})</m><br>\
+<m>f(2) = ${half}f(2 - f(1)) = ${half}f(2 - ${frac(1,8)}) = ${half}f(${frac(15,8)})</m><br>\
 First we need f(15/8).<br>\
-<m>f(\\frac{15}{8}) = \\frac{1}{2}f(\\frac{15}{8} - f(\\frac{7}{8}))</m><br>\
+<m>f(${frac(15,8)}) = ${half}f(${frac(15,8)} - f(${frac(7,8)}))</m><br>\
 Now we need f(7/8).<br>\
-<m>f(\\frac{7}{8}) = \\frac{1}{2}f(\\frac{7}{8} - f(\\frac{-1}{8})) = \\frac{1}{2}f(\\frac{7}{8} - \\frac{1}{8}) = \\frac{1}{2}f(\\frac{3}{4})</m><br><br><br>\
+<m>f(${frac(7,8)}) = ${half}f(${frac(7,8)} - f(${frac(-1,8)})) = ${half}f(${frac(7,8)} - ${frac(1,8)}) = ${half}f(${frac(3,4)})</m><br><br><br>\
 Okay, I think this is taking too long. f(7/8) will be some tiny number from all the multiplications by 1/2, so f(15/8) will be half of f(x) where x is barely smaller than 15/8. It will take too long to get even f(2). So let's generalize.<br><br><br>\
-<b>For 0 <= x < 1:</b><br>\
-<m>f(x) = \\frac{1}{2}f(x - f(x - 1))</m> but x - 1 < 0 so<br>\
-<m>f(x) = \\frac{1}{2}f(x - (1 - x)) = \\frac{1}{2}f(2x - 1)</m><br><br>\
+<b>For <m>0 <= x < 1</m>:</b><br>\
+<m>f(x) = ${half}f(x - f(x - 1))</m> but <m>x - 1 < 0</m> so<br>\
+<m>f(x) = ${half}f(x - (1 - x)) = ${half}f(2x - 1)</m><br><br>\
 Let's try finding f(7/8) with this.<br>\
-<m>f(\\frac{7}{8}) = \\frac{1}{2}f(\\frac{3}{4}) = \\frac{1}{4}f(\\frac{1}{2}) = \\frac{1}{8}f(0) = \\frac{1}{16}</m>. Each time we double our distance from 1 the function attains a new multiplier of <m>\\frac{1}{2}</m>. Based on this I'll declare this equation, verifiable with induction:<br>\
-<m>f(1 - \\frac{1}{2^{x}}) = \\frac{1}{2^{x+1}}</m>.<br><br>\
+<m>f(${frac(7,8)}) = ${half}f(${frac(3,4)}) = ${frac(1,4)}f(${frac(1,2)}) = ${frac(1,8)}f(0) = ${frac(1,16)}</m>. Each time we double our distance from 1 the function attains a new multiplier of <m>${half}</m>. Based on this I'll declare this equation, verifiable with induction:<br>\
+<m>f(1 - ${frac(1,"2<s>x</s>")} = ${frac(1,"2<s>x+1</s>")}</m>.<br><br>\
 Now applying that:<br>\
-<m>f(\\frac{15}{8}) = \\frac{1}{2}f(\\frac{15}{8} - \\frac{1}{16}) = \\frac{1}{2}f(\\frac{29}{16})</m><br>\
+<m>f(${frac(15,8)}) = ${half}f(${frac(15,8)} - ${frac(1,16)}) = ${half}f(${frac(29,16)})</m><br>\
 Hm. We'll need f(13/16) for that. Let's generalize some more.<br><br>\
-I'll leave it to you to prove that the function evaluated at a dyadic rational will always return a dyadic rational. I'm going to try to find <m>f(1 - \\frac{n}{2^{x}})</m> for <m>n < 2^{x}</m>, n odd.<br>\
+I'll leave it to you to prove that the function evaluated at a dyadic rational will always return a dyadic rational. I'm going to try to find <m>f(1 - ${frac(n,"2<s>x</s>")})</m> for <m>n < 2<s>x</s></m>, n odd.<br>\
 Starting at <m>1 - \\frac{n}{2^{x}}</m> we double our distance from 1 multiple times. Each time we attain another multiplication by \\frac{1}{2}.<br>\
 But how many times do we double our distance from 1 before the argument becomes negative?<br>\
-Well, once n > 2^y the argument will be negative. In other words y < <m>\\log_{2}{n}</m>, or specifically, y = <m>\\lfloor \\log_{2}{n} \\rfloor</m>. I'll call this <m>L2(n)</m>.<br><br>\
+Well, once <m>n > 2<s>y</s></m> the argument will be negative. In other words <m>y < log<u>2</u>n</m>, or specifically, y = <m>${lfl} log<u>2</u>n ${rfl}</m>. I'll call this <m>L2(n)</m>.<br><br>\
 So with <m>f(1 - \\frac{n}{2^{x}})</m> we do this division x - y times to get <m>\\frac{1}{2^{x - y}}f(1 - \\frac{n}{2^{y}}) = \\frac{\\frac{n}{2^{y}} - 1}{2^{x - y}} = \\frac{n - 2^{y}}{2^{x}}</m>.<br><br><br>\
 Now let's find a closed form for <m>f(2 - \\frac{n}{2^{x}})</m>.<br>\
 <m>f(2 - \\frac{n}{2^{x}}) = \\frac{1}{2}f(2 - \\frac{n}{2^{x}} - \\frac{n - 2^{y}}{2^{x}} = \\frac{1}{2}f(2 - \\frac{2n - 2^{y}}{2^x})</m>.<br><br>\
@@ -61,7 +65,7 @@ After b - a doublings, we get a new set S [a - 1, a - b + c, a - b + d...]<br>\
 We just removed one of the 1s in the binary expansion simply to decrease a by 1. Call the number of elements in the set, t. So we will get a down to at most a - t + 1 through this method.<br>\
 If a - t + 1 >= 0, in other words, if a >= t - 1, then the highest element, S[t] (starting from 1) will have taken S[t] - (a - t + 2) steps, so S[t] + t - a - 2 steps, plus an additional 2(a - t + 1) steps to reach 1. This totals S[t] + t - a - 2 + 2a - 2t + 2 = S[t] - t + a<br>\
 If a - t + 1 < 0, in other words, if a < t - 1, then we will not use the highest element S[t]. We will in fact only use a elements of S after a to get all the way to 1.<br>\
-"]
+`]
 }
 const urlPath = (new URL(window.location.href).search.substr(1));
 window.onload = function() {
