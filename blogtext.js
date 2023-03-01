d7bbad35827954513f448a80e3d85ec0ab50abc6
zfnQRZJT <102266031+zfnQRZJT@users.noreply.github.com>
2023-02-28 17:43:39 -0800

    Finished math formatting

--- blogtext.js
+++ blogtext.js
@@ -33,38 +33,39 @@ Now applying that:<br>\
 <m>f(${frac(15,8)}) = ${half}f(${frac(15,8)} - ${frac(1,16)}) = ${half}f(${frac(29,16)})</m><br>\
 Hm. We'll need f(13/16) for that. Let's generalize some more.<br><br>\
 I'll leave it to you to prove that the function evaluated at a dyadic rational will always return a dyadic rational. I'm going to try to find <m>f(1 - ${frac(n,"2<s>x</s>")})</m> for <m>n < 2<s>x</s></m>, n odd.<br>\
-Starting at <m>1 - \\frac{n}{2^{x}}</m> we double our distance from 1 multiple times. Each time we attain another multiplication by \\frac{1}{2}.<br>\
+Starting at <m>1 - ${frac(n,"2<s>x</s>")}</m> we double our distance from 1 multiple times. Each time we attain another multiplication by ${half}.<br>\
 But how many times do we double our distance from 1 before the argument becomes negative?<br>\
 Well, once <m>n > 2<s>y</s></m> the argument will be negative. In other words <m>y < log<u>2</u>n</m>, or specifically, y = <m>${lfl} log<u>2</u>n ${rfl}</m>. I'll call this <m>L2(n)</m>.<br><br>\
-So with <m>f(1 - \\frac{n}{2^{x}})</m> we do this division x - y times to get <m>\\frac{1}{2^{x - y}}f(1 - \\frac{n}{2^{y}}) = \\frac{\\frac{n}{2^{y}} - 1}{2^{x - y}} = \\frac{n - 2^{y}}{2^{x}}</m>.<br><br><br>\
-Now let's find a closed form for <m>f(2 - \\frac{n}{2^{x}})</m>.<br>\
-<m>f(2 - \\frac{n}{2^{x}}) = \\frac{1}{2}f(2 - \\frac{n}{2^{x}} - \\frac{n - 2^{y}}{2^{x}} = \\frac{1}{2}f(2 - \\frac{2n - 2^{y}}{2^x})</m>.<br><br>\
-It may be difficult to understand what exactly the transformation inside the function is that gives a coefficient of 1/2 outside the function. In the range [0,1) it was easy to understand: 2x - 1 doubles the distance from 1. What is this one doing?<br>\
-Let's look at some examples.<br>\
-(I will ignore the \"2 -\" part for now)<br>\
-<m>\\frac{141}{256} \\Rightarrow \\frac{154}{256} \\Rightarrow \\frac{180}{256} \\Rightarrow \\frac{232}{256} \\Rightarrow \\frac{336}{256}</m>.<br><br>\
-It's very unclear what this is doing. It finds the largest power of 2 less than n, then subtracts from 2n that power? It's easier to understand if I write it as n + n - 2^y. This is actually doubling n's distance from that power.<br><br>"/*
+So with <m>f(1 - \\frac{n}{2^{x}})</m> we do this division x - y times to get <m>${frac(1,"2^<s>x - y</s>"}f(1 - ${frac("n","2<s>y</s>")}) = ${frac(frac("n","2<s>y</s>") + " - 1","2<s>x - y</s>")} = ${frac("n - 2<s>y</s>","2<s>x</s>")}</m>.<br><br><br>\
+Now let's find a closed form for <m>f(2 - ${frac("n","2<s>x</s>")})</m>.<br>\
+<m>f(2 - ${frac("n","2<s>x</s>")}) = ${half}f(2 - ${frac("n","2<s>x</s>")} - ${frac("n - 2<s>y</s>","2<s>x</s>")}) = ${half}f(2 - ${frac("2n - 2<s>y</s>","2<s>x</s>")})</m>.<br><br>\
+It may be difficult to understand what exactly the transformation inside the function is that gives a coefficient of 1/2 outside the function. In the range <m>[0,1)</m> it was easy to understand: 2x - 1 doubles the distance from 1. What is this one doing?<br>\
+It finds the largest power of 2 less than n, then subtracts from 2n that power? It's easier to understand if I write it as n + n - 2^y. This is actually doubling n's distance from that power.<br><br>`
+/*Let's look at some examples.<br>\
+(I will ignore the "2 -" part for now)<br>\
+<m>141}{256} \\Rightarrow \\frac{154}{256} \\Rightarrow \\frac{180}{256} \\Rightarrow \\frac{232}{256} \\Rightarrow \\frac{336}{256}</m>.<br><br>\
+It's very unclear what this is doing."/*
 If n is a power of 2 this is actually easy. It will double the distance from n/2, leaving 3n/2, then it will double the distance from n, leaving 2n, and in general it will double every 2 steps. <br>\
 Now here's where <b>writing n in binary</b> will be a lifesaver. Given some number, like 10100001, this transformation leaves the first digit untouched, and doubles all digits after it.<br><br>\
-If n > 2^x/2 (as it is in the case of 141/256) it simply surpasses 2^x after 1 more than the number of 0's iterations. (Multiplying by 2 in binary moves a section of digits left 1 and adds a 0 at the end, just like multiplying by 10 works in base 10.) Let's write that as \\(y - L2(n - 2^{y})\\)<br><br>\
+If <m>n > 2^x/2 (as it is in the case of 141/256) it simply surpasses 2^x after 1 more than the number of 0's iterations. (Multiplying by 2 in binary moves a section of digits left 1 and adds a 0 at the end, just like multiplying by 10 works in base 10.) Let's write that as \\(y - L2(n - 2^{y})\\)<br><br>\
 If n < 2^x/2 it is a bit more complicated. Let's write n in binary as 1 [a zeroes] 1...<br>\
-After a doublings this will come to 11...<br>\*/+
-"Understanding the truly amazing complexity of this transformation is critical for the solution of this problem. But we can actually simplify for now.<br>\
+After a doublings this will come to 11...<br>*/+
+`Understanding the truly amazing complexity of this transformation is critical for the solution of this problem. But we can actually simplify for now.<br>\
 n starts as a power of 2, namely 1, in 2 - 1/8. In iteration 1 it will double the distance from n/2, giving 3n/2. Then it will double the distance from n, giving 2n. In general, it will double its distance from 2 every 2 steps, giving an additional multiplication of 1/4.<br>\
-We know that <m>f(2) = \\frac{1}{2}f(15/8)</m>. We can now see that this is <m>\\frac{1}{2}f(15/8) = \\frac{1}{8}f(14/8) = \\frac{1}{32}f(12/8) = \\frac{1}{128}f(8/8) = \\frac{1}{128}f(1) = \\frac{1}{128}\\frac{1}{8} = \\frac{1}{1024}</m>.]<br>\
-In general, <m>f(2 - \\frac{1}{2^{x}}) = \\frac{1}{2^{2x + 3}}</m>.<br>\
-<b>f(2) = 1/1024.</b><br><br><br>\
-Now it's time for our final stand. <m>f(2) = \\frac{1}{1024}</m>. <m>f(3) = \\frac{1}{2}f(3 - \\frac{1}{1024}) = \\frac{1}{2}f(\\frac{3071}{1024})</m>. This will relate to <m>f(2 - 1/1024) = \\frac{1}{2^{23}}</m><br>\
-Take a moment to think about that. f(3) is half of <m>f(3 - \\frac{1}{1024})</m> which is half of <m>f(3 - \\frac{1}{1024} - \\frac{1}{8388608})</m>. See how many steps it will take to get down to f(2) again? We should expect the answer for f(3) to be on the order of <m>2^{-10 000 000}</m>.\
+We know that <m>f(2) = ${half}f(${frac(15,8)})</m>. We can now see that this is <m>${frac(1,8)}f(${frac(14,8)}) = ${frac(1,32)}f(${frac(12,8)}) = ${frac(1,128)}f(${frac(9,8)}) = ${frac(1,128)}f(1) = ${frac(1,128)}${frac(1,8)} = ${frac(1,1024)}</m>.]<br>\
+In general, <m>f(2 - ${frac(1,"2<s>x</s>")}) = ${frac(1,"2<s>2x + 3</s>")}</m>.<br>\
+<b><m>f(2) = ${frac(1,1024)}.</m></b><br><br><br>\
+Now it's time for our final stand. <m>f(2) = ${frac(1,1024)}</m>. <m>f(3) = ${half}f(3 - ${frac(1,1024)})</m>. This will relate to <m>f(2 - ${frac(1,1024)}) = ${frac(1,"2<s>23</s>")}</m><br>\
+Take a moment to think about that. f(3) is a fourth of <m>f(3 - ${frac(1,1024)} - ${frac(1,8388608)})</m>. See how many steps it will take to get down to f(2) again? We should expect the answer for f(3) to be on the order of <m>2<s>-10 000 000</s></m>.\
 Let's try to figure that out. <b>We'll write it in binary.</b> It's <m>f(10.11111111101111111111) = f(11 - 0.00000000010000000000001).</m> When we double n's distance from the highest power of 2 less than n, we're really just doubling the digits after the first 1 in this small number. How many times does it take to get the small number above 1?<br>\
-<m>0.00000000010000000000001 \\Rightarrow\\Rightarrow\\Rightarrow\\Rightarrow\\Rightarrow\\Rightarrow 0.0000000001001 \\Rightarrow 0.000000000101 \\Rightarrow 0.00000000011 \\Rightarrow 0.000000001 \\Rightarrow\\Rightarrow 0.00000001 \\Rightarrow\\Rightarrow\\Rightarrow\\Rightarrow\\Rightarrow\\Rightarrow\\Rightarrow\\Rightarrow\\Rightarrow\\Rightarrow\\Rightarrow\\Rightarrow 0.01 \\Rightarrow\\Rightarrow 0.1 \\Rightarrow\\Rightarrow 1</m><br>\
-As you can see, it increases at a much greater rate when it is exactly a power of 2. If there is even 1 single 1 way after the 1st one, it has to bring that far-off 1 all the way to combine with the first 1 before it will get anywhere.<br><br>\
-Let's standardize that idea. Given ordered set [a,b,c,d...] how long will it take for <m>1/2^{a} + 1/2^{b} + 1/2^{c}...</m> to surpass 1, and how much will it surpass by?<br>\
-<i>This line of reasoning is arguably the critical piece of this entire problem. You can easily find f(2) manually. You cannot easily recurse millions of times to find f(3).</i><br><br><br>\
-After b - a doublings, we get a new set S [a - 1, a - b + c, a - b + d...]<br>\
+<m>0.00000000010000000000001 => => => => => => => => => => => => 0.00000000011 => 0.000000001 => => => => => => => => => => => => => => => => 0.1 => => 1</m><br>\
+As you can see, it increases at a much greater rate when it is exactly a power of 2. If there is even a single 1 way after the 1st one, it has to bring that far-off 1 all the way to combine with the first 1 before it will get anywhere.<br><br>\
+Let's standardize that idea. I'll use the notation <m>[a,b,c,d...]</m> to mean <m>${frac(1,"2<s>a</s>")} + ${frac(1,"2<s>b</s>")} + ${frac(1,"2<s>c</s>")}...</m>. How long will it take this to surpass 1, and how much will it surpass by?<br>\
+<u><i>This line of reasoning is arguably the critical piece of this entire problem. You can easily find f(2) manually. You cannot easily recurse millions of times to find f(3).</i></u><br><br><br>\
+After b - a doublings, we get a new set S = [a - 1, a - b + c, a - b + d...]<br>\
 We just removed one of the 1s in the binary expansion simply to decrease a by 1. Call the number of elements in the set, t. So we will get a down to at most a - t + 1 through this method.<br>\
-If a - t + 1 >= 0, in other words, if a >= t - 1, then the highest element, S[t] (starting from 1) will have taken S[t] - (a - t + 2) steps, so S[t] + t - a - 2 steps, plus an additional 2(a - t + 1) steps to reach 1. This totals S[t] + t - a - 2 + 2a - 2t + 2 = S[t] - t + a<br>\
-If a - t + 1 < 0, in other words, if a < t - 1, then we will not use the highest element S[t]. We will in fact only use a elements of S after a to get all the way to 1.<br>\
+<b>If <m>a - t + 1 >= 0</m></b>, in other words, if a >= t - 1, then the highest element, S[t] (starting from 1) will have taken S[t] - (a - t + 2) steps, so S[t] + t - a - 2 steps, plus an additional 2(a - t + 1) steps to reach 1. This totals <m>S[t] + t - a - 2 + 2a - 2t + 2 = S[t] - t + a steps, each multiplying the function by 1/2. The number is subtracted from 2 so this leaves us with 1, which we know is ${frac(1,8)}</m>.<br>\
+<b>If <m>a - t + 1 = -1</m></b>, if <m>a < t - 1</m>, then we will not use the highest element S[t]. We'll get 1.00...001. How many 0s? Well, on the previous step it was 0.1100...001. There are S[t] - S[t - 1] - 1 0s there. The final doubling adds 1 more 0 to this, giving S[t] - S[t - 1]. In other words <br>\
 `]
 }
 const urlPath = (new URL(window.location.href).search.substr(1));
