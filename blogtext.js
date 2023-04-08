56debdedda8ea2202fd980cc79e2718bc2b81084
zfnQRZJT <102266031+zfnQRZJT@users.noreply.github.com>
2023-04-08 09:31:32 -0700

    NegativeLnZero

--- blogtext.js
+++ blogtext.js
@@ -167,12 +167,13 @@ I'll see you next time."],
     \\[\\sum_{n=1}^{\\infty}\\frac{1}{n}\\]
     We know this is infinity. Can we use calculus to prove it?<br>
     Here's the key insight.
-    \\[\\frac{1}{1} + \\frac{1}{2} + \\frac{1}{3} + \\frac{1}{4} ... = \\frac{1^1}{1} + \\frac{1^2}{2} + \\frac{1^3}{3} + \\frac{1^4}{4} ... = \\f(1) - f(0), f(x) = \\frac{x^1}{1} + \\frac{x^2}{2} + \\frac{x^3}{3} + \\frac{x^4}{4} ...\\]
+    \\[\\frac{1}{1} + \\frac{1}{2} + \\frac{1}{3} + \\frac{1}{4} ... = \\frac{1^1}{1} + \\frac{1^2}{2} + \\frac{1^3}{3} + \\frac{1^4}{4} ... = f(1) - f(0), f(x) = \\frac{x^1}{1} + \\frac{x^2}{2} + \\frac{x^3}{3} + \\frac{x^4}{4} ...\\]
     Now we will take the derivative with the power rule.
     \\[f'(x) = \\frac{1x^0}{1} + \\frac{2x^1}{2} + \\frac{3x^2}{3} + \\frac{4x^3}{4} ... = 1 + x + x^2 + x^3 ... = \\frac{1}{1 - x}, x\\neq1\\]
     We are allowed to make that substitution because on all but one point of the domain, \\(|x| < 1\\).
-    \\[f'(x) = \\frac{1}{1-x}\\Rightarrow\\sum_{n=1}^{\\infty}\\frac{1}{n}=\\lim_{a\\rightarrow1^-}\\int_{0}^{a}\\frac{1}{1-x}dx\\]
-    There we have it. The sum becomes an integral.`]
+    \\[f'(x) = \\frac{1}{1-x}\\Longrightarrow\\sum_{n=1}^{\\infty}\\frac{1}{n}=\\lim_{a\\rightarrow1^-}\\int_{0}^{a}\\frac{1}{1-x}dx\\]
+    There we have it. The sum becomes an integral. It's actually simple to find:
+    \\[u = 1 - x\\]\\[\\lim_{a\\rightarrow0^+}\\int_{a}^{1}\\frac{1}{u}du=lim_{a\\rightarrow0^+}\\ln(x)\Big|_{\smalla}^{\small1}=-lim_{a\\rightarrow0^+}ln(a)=\infty\\]`]
 }
 const urlPath = (new URL(window.location.href).search.substr(1));
 window.onload = function() {
